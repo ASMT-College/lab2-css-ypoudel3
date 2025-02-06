9bdeeef50999c6ddaf8557fef9e2c21f83b5a2f4
ypoudel3 <yunishapoudel6@gmail.com>
2025-02-06 21:57:05 +0545

    Assignment

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://kit.fontawesome.com/70a81ab19a.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>
+    <div class="nav">
+        <div class="logo">LOGO</div>
+        <ul class="nav-list">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Post</a></li>
+        </ul>
+    </div>
+</header>
+<section>
+    <div class="center-container">
+    <div class="box">
+        <legend>Login</legend>
+        <br>
+        <input type="text" name="username" id="username" placeholder="Email or phone number">
+        <br><br>
+        <input type="password" name="password" id="password" placeholder="Password">
+        <br><br>
+        <button class="log"> LOG IN</button><br><br>
+        <h3><a href="">Forgot your password?</a></h3>
+        <h3>Don't have an account? <a href="signup.php">SignUp!</a></h3>
+    </div>
+    </div>
+</section>
+<footer>
+</footer>
+
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+<header>
+    <div class="nav">
+        <div class="logo">LOGO</div>
+        <ul class="nav-list">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Post</a></li>
+        </ul>
+    </div>
+</header>
+<section>
+    <div class="center-container">
+    <div class="box">
+        <legend>Create Account</legend><br>
+        <input type="text" name="fname" id="fname" placeholder="First Name">
+        <br><br>
+        <input type="text" name="lname" id="lname" placeholder="Last Name">
+        <br><br>
+        <input type="date" name="date" id="date" placeholder="Date of Birth">
+        <br><br>
+        <div class="radio-group">
+        <input type="radio" name="option" id="option1">
+        <label for="option1">Male</label>
+        </div>
+        <div class="radio-group">
+        <input type="radio" name="option" id="option2">
+        <label for="option2">Female</label>
+        </div>
+        <div class="radio-group">
+        <input type="radio" name="option" id="option3">
+        <label for="option3">Other</label>
+        </div>
+        <br>
+        <br>
+        <input type="email" name="email" id="email" placeholder="Email">
+        <br><br>
+        <input type="password" name="password" id="password" placeholder="Password">
+        <br><br>
+        <button class="log"> REGISTER</button><br><br>
+        <h3>Already have an account? <a href="login.php">Log In!</a></h3>
+</section>
